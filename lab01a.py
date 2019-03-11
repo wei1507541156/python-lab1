0e3f4e2cbd0ce95f753661f2f10c599f32efda35
ning <s206968@studenti.polito.it>
2019-03-11 17:38:29 +0100

    2nd commit

---
+++ lab01a.py
@@ -0,0 +1,12 @@
+
+a = input("plz input the number a=")
+b = input("plz input the number b=")
+result = int(a)+int(b)
+print("the result is " ,result)
+
+
+unknow = "flashbang"
+print(unknow[0],unknow[1],unknow[7],unknow[8])
+if len(unknow)<2:
+    print("it is too short")
+
